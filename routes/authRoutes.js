dcb52d05930c9efe063e5491e6dd17eb4033e3d9
Nwaiwuchi10 <djnchrys@gmail.com>
2023-06-09 05:03:51 -0700

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const { default: generateToken } = require("../Utils/generateToken");
 
 //REGISTER
 const transporter = nodemailer.createTransport({
@@ -75,6 +76,107 @@ router.post("/login", async (req, res) => {
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+//Forgotten password
+router.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+  try {
+    // 1. Check if the user with the given email exists in the database
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    ////nodemailer
+    const mailOptions = {
+      from: "djnchrys@gmail.com",
+      to: req.body.email,
+      subject: "Registration Successful",
+      html: `<p>Hello ${req.body.user.firstName},</p>
+      <p>You are about to reset Your account password.</p><p>Click <a href="http://your-app-url/reset-password/${user.token}">here</a> to confirm your password reset.</p>`,
+      //   text: "Congratulations, your registration was successful!",
+    };
+    // Generate a unique token and save it in the user document
+    const token = generateToken();
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = Date.now() + 3600000; // Token expires in 1 hour
+    await user.save();
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error("Error sending email:", err);
+      } else {
+        console.log("Email sent:", info.response);
+      }
+    });
+    // Send the password reset email
+    const resetUrl = `http://your-app-url/reset-password/${token}`;
+    sendPasswordResetEmail(email, resetUrl);
+
+    res.json({ message: "Password reset instructions sent to your email" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+router.get("/reset-password/:token", async (req, res) => {
+  const { token } = req.params;
+  try {
+    // Find the user with the provided token
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid or expired token" });
+    }
+
+    // Render the password reset form
+    res.render("reset-password", { token });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Forgotten password
+router.post("/reset-password", async (req, res) => {
+  const { token, password } = req.body;
+  try {
+    // Find the user with the provided token
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid or expired token" });
+    }
+    const mailOptions = {
+      from: "djnchrys@gmail.com",
+      to: req.body.email,
+      subject: "Registration Successful",
+      html: `<p>Hello ${req.body.user.firstName},</p>
+  <p>Your password has been reset sucessfully. Thanks</p>`,
+      //   text: "Congratulations, your registration was successful!",
+    };
+    // Update the user's password and clear the reset token fields
+    user.password = password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+    await user.save();
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error("Error sending email:", err);
+      } else {
+        console.log("Email sent:", info.response);
+      }
+    });
+    res.json({ message: "Password reset successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
